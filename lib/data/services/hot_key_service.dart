4fe0f1ce8375cfcccc832394fbbf5431eabd9a06
João Sereia <joao.lo.sereia@gmail.com>
2025-06-24 12:51:54 -0300

    feat(services/hot_key): add the hotkey service to manage application and system hotkeys

---
+++ lib/data/services/hot_key_service.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/services.dart';
+import 'package:hotkey_manager/hotkey_manager.dart';
+
+final class HotKeyService {
+  const HotKeyService();
+
+  Future<void> initialize() async => hotKeyManager.unregisterAll();
+
+  Future<void> register({
+    required KeyboardKey key,
+    required HotKeyScope scope,
+    List<HotKeyModifier> modifiers = const [],
+    VoidCallback? callback,
+  }) async {
+    final hotKey = HotKey(key: key, modifiers: modifiers, scope: scope);
+    await hotKeyManager.register(
+      hotKey,
+      keyDownHandler: (_) => callback?.call(),
+    );
+  }
+}
