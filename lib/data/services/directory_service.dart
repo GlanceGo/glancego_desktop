33596f4ef00c151fadf2ade66b0942f8b8a4951e
João Sereia <joao.lo.sereia@gmail.com>
2025-06-29 01:25:49 -0300

    feat(services/directory): add the directory service

---
+++ lib/data/services/directory_service.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+final class DirectoryService {
+  const DirectoryService();
+
+  Future<Directory> create(Uri path) async {
+    final dir = Directory(path.toFilePath());
+    if (!dir.existsSync()) await dir.create(recursive: true);
+    return dir;
+  }
+
+  Future<void> delete(Uri path) async {
+    final dir = Directory(path.toFilePath());
+    if (dir.existsSync()) await dir.delete(recursive: true);
+  }
+
+  Future<Directory> getTemp() async {
+    final result = await Future.wait([
+      getTemporaryDirectory(),
+      PackageInfo.fromPlatform(),
+    ]);
+
+    final tempDir = result[0] as Directory;
+    final package = result[1] as PackageInfo;
+
+    final path = p.join(tempDir.path, package.packageName);
+    final dir = await create(Uri.parse(path));
+
+    return dir;
+  }
+
+  Future<Directory> getAssets() async {
+    final mainExe = Platform.resolvedExecutable;
+    final mainDir = Directory(p.dirname(mainExe));
+
+    final path = p.join(mainDir.path, 'assets');
+    final dir = await create(Uri.parse(path));
+
+    return dir;
+  }
+}
