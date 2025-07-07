15d231f202501daad27ebc5bab12d1adc843b6d8
João Sereia <joao.lo.sereia@gmail.com>
2025-07-06 21:53:33 -0300

    feat(theme/metrics): add the animation curve and duration

--- lib/ui/core/theme/app_theme_metrics.dart
+++ lib/ui/core/theme/app_theme_metrics.dart
@@ -8,6 +8,8 @@ final class AppThemeMetricsImpl extends AppThemeMetrics {
     super.medium = 16,
     super.large = 24,
     super.icon = 24,
+    super.curve = Curves.easeInOut,
+    super.duration = const Duration(milliseconds: 200),
   });
 }
 
@@ -17,12 +19,16 @@ final class AppThemeMetrics extends ThemeExtension<AppThemeMetrics> {
     required this.medium,
     required this.large,
     required this.icon,
+    required this.curve,
+    required this.duration,
   });
 
   final double small;
   final double medium;
   final double large;
   final double icon;
+  final Curve curve;
+  final Duration duration;
 
   @override
   ThemeExtension<AppThemeMetrics> copyWith({
@@ -30,12 +36,16 @@ final class AppThemeMetrics extends ThemeExtension<AppThemeMetrics> {
     double? medium,
     double? large,
     double? icon,
+    Curve? curve,
+    Duration? duration,
   }) {
     return AppThemeMetrics(
       small: small ?? this.small,
       medium: medium ?? this.medium,
       large: large ?? this.large,
       icon: icon ?? this.icon,
+      curve: curve ?? this.curve,
+      duration: duration ?? this.duration,
     );
   }
 
@@ -51,6 +61,8 @@ final class AppThemeMetrics extends ThemeExtension<AppThemeMetrics> {
       medium: lerpDouble(medium, other.medium, t)!,
       large: lerpDouble(large, other.large, t)!,
       icon: lerpDouble(icon, other.icon, t)!,
+      curve: t < 0.5 ? curve : other.curve,
+      duration: t < 0.5 ? duration : other.duration,
     );
   }
 }
